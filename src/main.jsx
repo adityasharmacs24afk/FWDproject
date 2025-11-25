b10a2f8d4f0c2c4518c42ccc24efb84498943bc7
Adityasharma0810 <adityasharma08.2006@gmail.com>
2025-11-25 23:07:11 +0530

    login page made

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";                // your landing page component
+import Login from "./Pages/login.jsx";      // adjust path if your login file is elsewhere
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      {/* optional: add signup route if you have it */}
+      {/* <Route path="/signup" element={<Signup />} /> */}
+    </Routes>
+  </BrowserRouter>
+);
