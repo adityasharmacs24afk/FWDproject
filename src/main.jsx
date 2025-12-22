4d77bc657682c747fee0a9b9eee9b7bffe95bc65
Adityasharma0810 <adityasharma08.2006@gmail.com>
2025-12-22 18:29:06 +0530

    messages done version 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InvestorReports from "./Pages/InvestorReports.jsx";
 import InvestorExplore from './Pages/InvestorExplore';
-
+import FounderMessages from './Pages/FounderMessages';
 import App from "./App.jsx";                     
 import Login from "./Pages/login.jsx";           
 import Signup from "./Pages/signup.jsx";          
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />   
       <Route path="/investor-explore" element={<InvestorExplore />} />
-
+      <Route path="/founder-messages" element={<FounderMessages />} />
       <Route path="/investor-dashboard" element={<InvestorDashboard />} />
       <Route path="/investor-feed" element={<InvestorFeed />} />
       <Route path="/investor-profile" element={<InvestorProfile />} />
